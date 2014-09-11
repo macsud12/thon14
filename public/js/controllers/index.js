fc485114fbd1e0ad7984334694104e198f8fef74
Sevrugin, IIya <lithium366@gmail.com>
2014-09-11 17:31:33 -0400

    Bootstrap css added

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,11 +1,6 @@
 hackathon.app.controller('usersList', function ($scope) {
 
   $scope.title =  "Users list";
-  $scope.users = [
-    { name: "test1" },
-    { name: "test2" },
-    { name: "test3" }
-  ];
 
   hackathon.socket.on('usersUpdated', function(data){
     $scope.users = data;
